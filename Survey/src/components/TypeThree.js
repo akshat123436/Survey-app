2649222537a5c3bdc9b6e2903c37c6e43158d69e
Akshat Kothavade <akshat123436@gmail.com>
2023-01-25 08:27:33 +0530

    added the type three question body

--- Survey/src/components/TypeThree.js
+++ Survey/src/components/TypeThree.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-function TypeThree() {
-  return <div>TypeThree</div>;
+function TypeThree(props) {
+  const choices = props.question.choices.map((choice) => (
+    <Fragment key={choice.id}>
+      <input
+        type="checkbox"
+        value={choice.id}
+        id={choice.id}
+        name={props.question.id}
+      ></input>
+      <label htmlFor={choice.id}>{choice.choice_text}</label>
+    </Fragment>
+  ));
+  return (
+    <Fragment>
+      <h2>{props.question.question_text}</h2>
+      {choices}
+    </Fragment>
+  );
 }
 
 export default TypeThree;
