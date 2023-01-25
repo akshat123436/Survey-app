de6646b4b44da78462c65453c6cae633f762c432
Akshat Kothavade <akshat123436@gmail.com>
2023-01-25 08:31:49 +0530

    added type four question body

--- Survey/src/components/TypeFour.js
+++ Survey/src/components/TypeFour.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-function TypeFour() {
-  return <div>TypeFour</div>;
+function TypeFour(props) {
+  return (
+    <Fragment>
+      <h2>{props.question.question_text}</h2>
+      <input type="text" name={props.question.id} />
+    </Fragment>
+  );
 }
 
 export default TypeFour;
